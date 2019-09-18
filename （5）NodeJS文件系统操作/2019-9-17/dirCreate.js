03abd8d097a74c375da5f16527dc80d027541c9c
孙丽颖 <33644965+sunliying123@users.noreply.github.com>
2019-09-18 15:12:52 +0800

    Add files via upload

---
+++ （5）NodeJS文件系统操作/2019-9-17/dirCreate.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+fs.mkdirSync("hello");
+
+var list = fs.readdirSync(__dirname);
+console.log(list);
+
+var files = fs.readdirSync(__dirname, "/node");
+fs.unlinkSync(path.join(__dirname, "/node/" + files[0]));
+fs.rmdirSync(path.join(__dirname, "/node"));
